a58de2a64f882b1b74a0de0695ecb0d7270365d5
Sachin Mahajan <sachinm78@gmail.com>
2019-06-17 21:54:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 let element = document.createElement('div');
-undefined
+
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-"#f9f9f9"
+
 document.body.appendChild(element);
 <div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, DOM!​</div>​
+
 element.style.textAlign = 'center';
-"center"
+
+
 let ul = document.createElement('ul');
  
 for (let i = 0; i < 3; i++) {
